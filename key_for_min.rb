5e04e39f753c371e5edd6902d0f2ba9208b1d069
Sharad Satsangi <s.satsangi@gmail.com>
2020-09-14 02:39:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,15 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+#There's gotta be an easier way.  A way
+#to do it with one #each or something.
+#but since we aren't using the enummethods
+#that make this easy, I have no idea how to
+#find a min in one #each. without setting 
+# a min_value outside the loop to something
+#so big I hope no one would have it as a value.
+# That seems like a hacky thing, so this is 
+#how I'm doing it
 def key_for_min_value(name_hash)
-
+  array_to_search
 end
